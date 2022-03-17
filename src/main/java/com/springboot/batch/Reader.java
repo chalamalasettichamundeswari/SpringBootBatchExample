774168a79feead2216ff6e80262436f76c8fdedc
“chalamalasettichamundeswari” <chalamalasettichamundeswari@gmail.com>
2022-03-17 22:38:44 +0530

    Commit After All files

---
+++ src/main/java/com/springboot/batch/Reader.java
@@ -0,0 +1,23 @@
+package com.springboot.batch;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+public class Reader implements ItemReader<String> {
+    private String[]  courses = {"spring","springboot","react"};
+	private int count;
+	@Override
+	public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+		System.out.println("Inside Read Method");
+		if(count<courses.length) {
+			return courses[count++];
+		}else {
+			count=0;
+		}
+		return null;
+	}
+
+}
+
